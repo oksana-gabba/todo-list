8fab92f2dafa8032e33d649cad5049544001fc49
Gabbasova Oksana <oksana.gabba@gmail.com>
2020-10-29 13:14:14 +0200

    Updates

---
+++ precache-manifest.ca7393b93211e3a62df61e2782bbb827.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02ecedcfcf094c641cea5fc4dd8f9ad",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "08c7c792eeff4d5e843f",
+    "url": "/todo-list/static/css/main.a688b7cb.chunk.css"
+  },
+  {
+    "revision": "0a43c0b93dea43fdf004",
+    "url": "/todo-list/static/js/2.5c13bd5a.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/todo-list/static/js/2.5c13bd5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c7c792eeff4d5e843f",
+    "url": "/todo-list/static/js/main.255c828f.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  },
+  {
+    "revision": "7cc3ec331b1589de396eb7db19e62bd0",
+    "url": "/todo-list/static/media/check-mark.7cc3ec33.svg"
+  },
+  {
+    "revision": "0586c3c6b3adb795af0c6a90fccf6d10",
+    "url": "/todo-list/static/media/dustbin.0586c3c6.svg"
+  },
+  {
+    "revision": "4d8cd2e26d46c0dc8082b2ee9f7a2232",
+    "url": "/todo-list/static/media/plus.4d8cd2e2.svg"
+  }
+]);
